c840010594c338b755977c0d36c8c8776a17ab62
michi-strichi <michael.j.hochreiter@gmail.com>
2025-05-23 19:52:53 +0200

    update about page

--- src/components/About.js
+++ src/components/About.js
@@ -52,45 +52,36 @@ const About = ({ theme, min781, aboutHintEnabled, setAboutHintEnabled }) => {
                     <h1>
                         Hi ✌️ I'm Michael
                     </h1>
-                    <h4>
-                        <span className="C">C</span>urrently I'm studying media technology &amp; design in Hagenberg and am really digging it. I come from a 3D background, but interactive media has found its way into my interests. Now I work in all the places where interactivity meets the three dimensions.<br />
-                        When I'm not playing around with 3D models, I'm probably either cooking pasta, blasting music way too loud or hiking through some forest.
-                    </h4>
 
                     <h3>
                         Work Experience
                     </h3>
                     <p>
-                        Creative Development Intern at <a href='https://ravespace.io' target='_blank' rel="noreferrer">RAVE.SPACE</a>, Berlin
+                        full stack developer at <a href='https://vrisch.com' target='_blank' rel="noreferrer">vrisch</a>, Vienna
                         <br />
-                        Interaction Developer at <a href='https://www.re-spaces.com' target='_blank' rel="noreferrer">Responsive Spaces</a>, Linz
+                        webGL/webXR developer at <a href='https://ravespace.io' target='_blank' rel="noreferrer">RAVE.SPACE</a>, Berlin
                         <br />
-                        3D Design Intern at <a href='https://www.crazyeye.at' target='_blank' rel="noreferrer">Crazy Eye</a>, Vienna
+                        interaction developer at <a href='https://www.re-spaces.com' target='_blank' rel="noreferrer">Responsive Spaces</a>, Linz
+                        <br />
+                        3D design intern at <a href='https://www.crazyeye.at' target='_blank' rel="noreferrer">Crazy Eye</a>, Vienna
                     </p>
 
                     <h3>
                         Current Reads
                     </h3>
                     <p>
-                        <span className="Book">Confessions of a Mask</span> by Yukio Mishima
+                        <span className="Book">I who have never known men</span> by Jacqueline Harpman
                         <br />
-                        <span className="Book">A (Very) Short History of Life on Earth</span> by Henry Gee
+                        <span className="Book">Confessions of a Mask</span> by Yukio Mishima
                         <br />
                         <span className="Book">Star Maker</span> by Olaf Stapledon
                     </p>
 
                     <h3>
-                        Favourite Animals
+                        Favourite Animal
                     </h3>
                     <p>
-                        Cicada sitting on a toad's back
-                    </p>
-
-                    <h3>
-                        My Mantra
-                    </h3>
-                    <p>
-                        "Observe the world like a child and design it like an adult"
+                        Literally any insect... 🦋🐝🐞🦟🐜🪲
                     </p>
                 </div>
             </div>
